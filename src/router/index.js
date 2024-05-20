2a9dc01c65b85417c8d996a22363f6468f263376
utkirbek <utkirbektoirov1@gmail.com>
2024-05-20 11:04:12 +0500

    commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import ExprementView from "../views/Exprement.vue";
+import ExprementView from "../views/ExprementView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
